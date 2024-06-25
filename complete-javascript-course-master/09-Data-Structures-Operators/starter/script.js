baabbb2646c1b695a6b31e2645eb1e6128bd3ddf
sanina13 <57276404+sanina13@users.noreply.github.com>
2024-06-25 10:29:12 +0100

    update

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -52,7 +52,9 @@ const restaurant = {
   },
 };
 
-const game = {
+//Sets
+
+/* const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
   players: [
@@ -94,32 +96,32 @@ const game = {
 };
 /*1. Loop over the game.scored array and print each player name to the console,
 along with the goal number (Example: "Goal 1: Lewandowski")*/
-for (const [key, player] of game.scored?.entries()) {
+/* for (const [key, player] of game.scored?.entries()) {
   console.log(`Goal ${key + 1}: ${player}`);
-}
+} */
 /*2. Use a loop to calculate the average odd and log it to the console (We already
-studied how to calculate averages, you can go check if you don't remember)*/
-const values = Object.values(game.odds);
+studied how to calculate averages, you can go check if you don't remember)/* */
+/* const values = Object.values(game.odds);
 let sum = 0;
 for (const odd of values) {
   sum += odd;
 }
-const average = sum / values.length;
+const average = sum / values.length; */
 /*3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
 Odd of victory Bayern Munich: 1.33
-Odd of draw: 3.25
+/* Odd of draw: 3.25
 Odd of victory Borrussia Dortmund: 6.5
 Get the team names directly from the game object, don't hardcode them
 (except for "draw"). Hint: Note how the odds and the game objects have the
 same property names */
-const entries = Object.entries(game.odds);
+/* const entries = Object.entries(game.odds);
 for (const [key, value] of entries) {
   if (key === 'x') {
     console.log(`Odd of draw: ${value}`);
   } else {
     console.log(`Odd of victory ${game[key]}: ${value}`);
-  }
-}
+  } */
+/* } */
 /*4. Bonus: Create an object called 'scorers' which contains the names of the
 players who scored as properties, and the number of goals as the value. In this
 game, it will look like this:
@@ -129,13 +131,13 @@ Gnarby: 1,
 Hummels: 1,
 Lewandowski: 2 */
 
-const scorers = {};
+/* const scorers = {};
 
 for (const player of game.scored) {
   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
 }
 
-console.log(scorers);
+console.log(scorers); */
 
 /*
 1. Loop over the game.scored array and print each player name to the console,
