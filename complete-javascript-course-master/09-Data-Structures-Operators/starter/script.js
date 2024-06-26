788e7828b3d29f2fb48cd1a03850e220edb7ed5b
sanina13 <57276404+sanina13@users.noreply.github.com>
2024-06-26 09:58:32 +0100

    Maps

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -52,9 +52,40 @@ const restaurant = {
   },
 };
 
+//maps
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+//chain set to a map
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are opne :D')
+  .set(false, 'We are closed :(');
+
+const time = 21;
+rest.get(time > rest.get('open') && time < rest.get('close'));
+
+console.log(rest.has('categories'));
+
+rest.delete(2);
+console.log(rest);
+
+console.log(rest.size);
+/* rest.clear(); */
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.get(arr));
+
 //Sets
 
-const ordersSet = new Set([
+/* const ordersSet = new Set([
   'Pasta',
   'Pizza',
   'Pizza',
@@ -81,7 +112,7 @@ const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
 const staffUnique = [...new Set(staff)];
 console.log(staffUnique);
 const staffArr = [...staffUnique];
-console.log(staffArr);
+console.log(staffArr); */
 
 /* const game = {
   team1: 'Bayern Munich',
