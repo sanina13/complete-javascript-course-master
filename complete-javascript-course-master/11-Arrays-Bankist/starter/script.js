e48f7480a4fbb077459e180260f25dc04fecc8b1
sanina13 <57276404+sanina13@users.noreply.github.com>
2024-06-28 16:48:09 +0100

    arrays at

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -71,46 +71,80 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
 //Array simple methods
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr = ['a', 'b', 'c', 'd', 'e'];
 
-//slice
+// //slice
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1, -2));
+// //copy array with slice
+// console.log(arr.slice());
 
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
-//copy array with slice
-console.log(arr.slice());
+// //splice method
+// // Funciona como o slice só que modifica o array removendo ao array os valores spliced!
+// //splice is good to delete elem from the array
+// console.log(arr.splice(2));
+// console.log(arr);
+// //ex of deleting the last elem of the array with splice
+// arr.splice(-1);
+// // DIFFETENCE BETWEEN IS THE SECOND ELEM OF SPLICE IS THE NUMBER OF ELEM WE WANT TO DELETE!
 
-//splice method
-// Funciona como o slice só que modifica o array removendo ao array os valores spliced!
-//splice is good to delete elem from the array
-console.log(arr.splice(2));
-console.log(arr);
-//ex of deleting the last elem of the array with splice
-arr.splice(-1);
-// DIFFETENCE BETWEEN IS THE SECOND ELEM OF SPLICE IS THE NUMBER OF ELEM WE WANT TO DELETE!
+// // REVERSE
+// //REVERSE MUTATE THE ARRAY! IMPORTANT!
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
 
-// REVERSE
-//REVERSE MUTATE THE ARRAY! IMPORTANT!
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
+// // CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// // this do the same thing than with the spread operator
+// console.log([...arr, ...arr2]);
 
-// CONCAT
+// // JOIN
+// console.log(letters.join(' - '));
 
-const letters = arr.concat(arr2);
-console.log(letters);
-// this do the same thing than with the spread operator
-console.log([...arr, ...arr2]);
+//The new at Method
 
-// JOIN
-console.log(letters.join(' - '));
+// // at
+// const arr = [23, 11, 64];
+// console.log(arr[0]);
+// console.log(arr.at(0)); // do the same as arr[0]
+
+// //getting last array element
+// console.log(arr[arr.length - 1]); // last elem
+// console.log(arr.slice(-1)[0]); // last elem
+// console.log(arr.at(-1)); // with at we can put negative index and do what we do in the slice method, and this is a shorter way to get the last elem
+
+// // at on strings
+// console.log('jonas'.at(0));
+
+//FOR EACH
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+//   if (movement > 0) {
+//     console.log(`You deposited ${movement}`);
+//   } else {
+//     console.log(`You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// For EACH
+
+movements.forEach(function (movement) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+});
