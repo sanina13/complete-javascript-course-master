867f5570bd7c036535bf0d4061b1e89fd1278de6
sanina13 <57276404+sanina13@users.noreply.github.com>
2024-07-02 17:43:01 +0100

    Numbers

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,12 +223,13 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
     );
     console.log(index);
+    console.log(Math.trunc());
     // .indexOf(23)
 
     // Delete account
@@ -251,3 +252,32 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+console.log(23 === 23.0);
+
+//Base 10 - 0 to 9. 1/10 = 0.1. 3/10 = 3.333333333
+//Binary base 2 - 0.1
+
+console.log(0.1 + 0.2);
+
+//String to a number
+console.log(Number('23'));
+console.log(+'23'); // String to number in a cleaner way
+
+//Parsing
+console.log(Number.parseInt('30px')); //needs to start with a number to work
+console.log(Number.parseInt('30px', 10)); // We can define as 2 arg the base of the number we are parsing into a integer
+
+console.log(Number.parseFloat('2.5rem'));
+console.log(Number.parseFloat('2.5rem'));
+
+//Check if value is NaN
+console.log(Number.isNaN('20'));
+
+//best way to check if a value is a Number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite(+'20x'));
+console.log(Number.isFinite(0));
+
+// for integers
+console.log(Number.isInteger(23));
