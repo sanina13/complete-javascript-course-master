eca5cb9e11681dfaf7407d8264e3132087c69985
sanina13 <57276404+sanina13@users.noreply.github.com>
2024-07-02 14:57:53 +0100

    Challenge #4

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -593,23 +593,80 @@ btnSort.addEventListener('click', function (e) {
 //5522.00000001
 
 // 4. Convert any string to title case
-const convertTitleCase = function (title) {
-  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+// const convertTitleCase = function (title) {
+//   const capitalize = str => str[0].toUpperCase() + str.slice(1);
 
-  const expections = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
-  title = title
-    .toLowerCase()
-    .split(' ')
-    .map(word => {
-      if (!expections.includes(word)) {
-        return capitalize(word);
-      } else {
-        return word;
-      }
-    })
-    .join(' ');
-  return capitalize(title);
+//   const expections = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+//   title = title
+//     .toLowerCase()
+//     .split(' ')
+//     .map(word => {
+//       if (!expections.includes(word)) {
+//         return capitalize(word);
+//       } else {
+//         return word;
+//       }
+//     })
+//     .join(' ');
+//   return capitalize(title);
+// };
+// console.log(convertTitleCase('this is a nice title'));
+// console.log(convertTitleCase('this is a LONG title but not too long'));
+// console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+// # CHALLENGE #4
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+const recommendedFood = dogs.forEach(
+  dog => (dog.foodPortion = Math.trunc(dog.weight ** 0.75 * 28))
+);
+
+console.log(dogs);
+// Create an array containing all owners of dogs who eat too much
+// ('ownersEatTooMuch') and an array with all owners of dogs who eat too little
+// ('ownersEatTooLittle').
+
+const sarahDog = dogs.find(dog => dog.owners.includes('Sarah'));
+
+const checkFood = function (dog) {
+  const perFood = dog.foodPortion * 0.1;
+  if (
+    dog.curFood <= dog.foodPortion + perFood &&
+    dog.curFood >= dog.foodPortion - perFood
+  ) {
+    return 'perfect';
+  } else {
+    if (dog.curFood > dog.foodPortion + perFood) {
+      return 'too much';
+    } else {
+      return 'too little';
+    }
+  }
 };
-console.log(convertTitleCase('this is a nice title'));
-console.log(convertTitleCase('this is a LONG title but not too long'));
-console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+checkFood(sarahDog);
+console.log(sarahDog);
+
+const ownersEatTooMuch = dogs
+  .filter(dog => checkFood(dog) === 'too much')
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch.join(' and ') + ' dogs eat too much!');
+
+const ownersEatTooLittle = dogs
+  .filter(dog => checkFood(dog) === 'too little')
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle.join(' and ') + ' dogs eat too little!');
+
+console.log(dogs.some(dog => dog.foodPortion === dog.curFood));
+console.log(dogs.some(dog => checkFood(dog) === 'perfect'));
+
+const okayDogs = dogs.filter(dog => checkFood(dog) === 'perfect');
+console.log(okayDogs);
+
+//sort
+const sortedArr = dogs.slice().sort((a, b) => a.foodPortion - b.foodPortion);
+console.log(sortedArr);
