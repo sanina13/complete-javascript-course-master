bd6af697ebdc88cbc93e0fbfdf6362813c08cc3e
sanina13 <57276404+sanina13@users.noreply.github.com>
2024-07-03 08:59:00 +0100

    Math

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -253,31 +253,80 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
-console.log(23 === 23.0);
+// console.log(23 === 23.0);
 
-//Base 10 - 0 to 9. 1/10 = 0.1. 3/10 = 3.333333333
-//Binary base 2 - 0.1
+// //Base 10 - 0 to 9. 1/10 = 0.1. 3/10 = 3.333333333
+// //Binary base 2 - 0.1
 
-console.log(0.1 + 0.2);
+// console.log(0.1 + 0.2);
 
-//String to a number
-console.log(Number('23'));
-console.log(+'23'); // String to number in a cleaner way
+// //String to a number
+// console.log(Number('23'));
+// console.log(+'23'); // String to number in a cleaner way
 
-//Parsing
-console.log(Number.parseInt('30px')); //needs to start with a number to work
-console.log(Number.parseInt('30px', 10)); // We can define as 2 arg the base of the number we are parsing into a integer
+// //Parsing
+// console.log(Number.parseInt('30px')); //needs to start with a number to work
+// console.log(Number.parseInt('30px', 10)); // We can define as 2 arg the base of the number we are parsing into a integer
 
-console.log(Number.parseFloat('2.5rem'));
-console.log(Number.parseFloat('2.5rem'));
+// console.log(Number.parseFloat('2.5rem'));
+// console.log(Number.parseFloat('2.5rem'));
 
-//Check if value is NaN
-console.log(Number.isNaN('20'));
+// //Check if value is NaN
+// console.log(Number.isNaN('20'));
 
-//best way to check if a value is a Number
-console.log(Number.isFinite(20));
-console.log(Number.isFinite(+'20x'));
-console.log(Number.isFinite(0));
+// //best way to check if a value is a Number
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite(+'20x'));
+// console.log(Number.isFinite(0));
 
-// for integers
-console.log(Number.isInteger(23));
+// // for integers
+// console.log(Number.isInteger(23));
+
+// //Raizes
+// console.log(Math.sqrt(25));
+// //another way is:
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
+
+// //
+
+// console.log(Math.max(5, 18, 23, 11, 2, '30')); //Return the max value included when have a string with a number but this not parsing, if we put '30px' will return NaN
+
+// console.log(Math.min(4, 2, 53, 23));
+
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// console.log(Math.trunc(Math.random() * 6) + 1);
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min) + 1) + min;
+// console.log(10, 20);
+
+// //Rounding Integers
+// console.log(Math.trunc(23.3));
+
+// //round to the nearest Integer
+// console.log(Math.round(23.3));
+// console.log(Math.round(23.9));
+
+// //ceil round up always
+// console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
+
+// // floor round down alwayss
+// console.log(Math.floor(23.3));
+// console.log(Math.floor(23.9));
+
+// // TRUNC VS FLOOR
+// console.log(Math.trunc(-23.3)); // here he just retire 0.3
+// console.log(Math.floor(-23.3)); // here he go for -24 because is the nearsted integer
+// //FLOOR IS BETTER IN THE CASE OF GET A RANDOM INT!
+
+// //Rounding decimals
+// console.log((2.7).toFixed(0)); // toFixed will return always a String
+// console.log((2.7).toFixed(3));
+// console.log((2.345).toFixed(2));
+// console.log(+(2.345).toFixed(2));
+
+//Remainder Operator
+console.log(5 % 2);
