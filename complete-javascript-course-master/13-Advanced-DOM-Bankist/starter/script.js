72c67aac52d295c1c2af61824ba65fec5adeb21e
sanina13 <57276404+sanina13@users.noreply.github.com>
2024-07-05 09:49:30 +0100

    Advanced DOM

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener('click', openModal);
+});
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +31,42 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// SELECTING, CREATING AND DELETING ELEMENTS
+
+console.log(document.documentElement); // documentElement applies to all the page
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSection = document.querySelectorAll('.section');
+console.log(allSection); //return a node list
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button'); // returns a HTML Collection, is always updated when we remove in this example a button, this collection updates immediatly
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn')); // return a HTML collection too
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent =
+//   'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// header.prepend(message); //first child of the header
+// header.append(message); //last child of the header
+header.append(message); //last child of the header
+// header.append(message.cloneNode(true)); //clone the message and append twice the same message
+
+// header.before(message); //before the header
+// header.after(message); //after the header
+
+//Deleting elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
