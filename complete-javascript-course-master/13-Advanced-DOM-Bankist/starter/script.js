a5e02433dedbba7dc1ec91acd6c2eecd52474210
sanina13 <57276404+sanina13@users.noreply.github.com>
2024-07-06 08:17:01 +0100

    smoth scrolling, events

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -32,43 +32,141 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// SELECTING, CREATING AND DELETING ELEMENTS
+// // SELECTING, CREATING AND DELETING ELEMENTS
 
-console.log(document.documentElement); // documentElement applies to all the page
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement); // documentElement applies to all the page
+// console.log(document.head);
+// console.log(document.body);
 
-const header = document.querySelector('.header');
-const allSection = document.querySelectorAll('.section');
-console.log(allSection); //return a node list
+// const header = document.querySelector('.header');
+// const allSection = document.querySelectorAll('.section');
+// console.log(allSection); //return a node list
 
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button'); // returns a HTML Collection, is always updated when we remove in this example a button, this collection updates immediatly
-console.log(allButtons);
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button'); // returns a HTML Collection, is always updated when we remove in this example a button, this collection updates immediatly
+// console.log(allButtons);
 
-console.log(document.getElementsByClassName('btn')); // return a HTML collection too
+// console.log(document.getElementsByClassName('btn')); // return a HTML collection too
 
-// Creating and inserting elements
-// .insertAdjacentHTML
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-// message.textContent =
-//   'We use cookies for improved functionality and analytics.';
-message.innerHTML =
-  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
-// header.prepend(message); //first child of the header
+// // Creating and inserting elements
+// // .insertAdjacentHTML
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// // message.textContent =
+// //   'We use cookies for improved functionality and analytics.';
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// // header.prepend(message); //first child of the header
+// // header.append(message); //last child of the header
 // header.append(message); //last child of the header
-header.append(message); //last child of the header
-// header.append(message.cloneNode(true)); //clone the message and append twice the same message
+// // header.append(message.cloneNode(true)); //clone the message and append twice the same message
 
-// header.before(message); //before the header
-// header.after(message); //after the header
+// // header.before(message); //before the header
+// // header.after(message); //after the header
 
-//Deleting elements
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    // message.remove(); // new way to remove
+// //Deleting elements
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     // message.remove(); // new way to remove
 
-    message.parentElement.removeChild(message); //old way to remove
-  });
+//     message.parentElement.removeChild(message); //old way to remove
+//   });
+
+// // STYLES
+// //inline styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+
+// //get the styles of a elem
+
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+
+// // with the line we have below we can change all the style of a page with a simple line, in this case we go for root, elem, style, set proprety with the var of css and set all the color to orangered
+// // document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// //Attributtes ex: p, a, div all of the elem of html
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+
+// console.log(logo.className);
+
+// // alt and src and className are standart and this works because of this, but "se criarmos uma propriedade nova que não seja standart não irá retornar undefined!"
+
+// logo.alt = 'Beautiful minimalist logo'; // this CHANGE!
+
+// // Non-standard
+// console.log(logo.designer);
+// console.log(logo.getAttribute('designer')); // Conseguimos assim obter o mesmo que nos standard, e dar return ao que está dentro da propriedade
+// logo.setAttribute('company', 'Bankist'); // Cria o atributo company dentro de logo com o valor de bankist
+
+// console.log(logo.src); // returns the absolute URL if we want just de src we need the get attribute
+// console.log(logo.getAttribute('src')); // Desta forma RELATIVE FORM
+
+// //É igual as img os links, o primeiro irá retornar o absoluto e o segundo relativo, pode ser util muitas vezes o relativo tambem
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+// //DATA attributes
+// console.log(logo.dataset.versionNumber); // we use camel case because is version_number = versionNumber
+
+// //Classes
+// logo.classList.add('a', 'b');
+// logo.classList.remove('a', 'b');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
+
+// //DONT USE THIS THIS WILL OVERWRITE ALL OF THE CLASSES!
+// logo.className = 'jonas'
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect(); // get the postion of the section 1
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset); // we get scroll position with that
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  //Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+  //OLD SCHOLL WAY
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  //New way more easly
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//Types of eventes and Handlers
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+
+  // h1.removeEventListener('mouseenter', alertH1); // we need to export the function from inside the add eventlistener and then we remove the event
+};
+
+const h1 = document.querySelector('h1');
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
