d0c4b48fdb485e08a7406d0fc57016e1bb6af550
sanina13 <57276404+sanina13@users.noreply.github.com>
2024-07-13 12:08:50 +0100

    OOP

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+//Constructer Functions and the new Operator
+
+//constructer function always with capital letter
+const Person = function (firsName, birthYear) {
+  //Instance properties
+  this.firsName = firsName;
+  this.birthYear = birthYear;
+
+  this.calcAge = function () {
+    console.log(2024 - this.birthYear);
+  };
+};
+
+//Jonas is a instance of Person
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+//And matilda and jack are to instance of Person
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person); //verify if jonas is a instance of Person
